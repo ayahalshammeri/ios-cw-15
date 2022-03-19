e299831d34617dde9c60713c2f4fde33c541bedb
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-19 10:34:36 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
+
+struct student {
+    var name: String
+    var age: Int
+    var grade:Double
+    var attendence: Bool
+}
+var Ayah = student(name: "Ayah", age: 17, grade: 100,attendence: false)
